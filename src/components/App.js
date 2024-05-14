8b93ed62884ef0bb8bddb1732bd2e09a50fd2975
61962059 <sunlift10000@gmail.com>
2024-05-14 11:27:12 +0800

    navigation

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppShell from './AppShell';
+
+class App extends React.Component{
+    render() {
+        return(
+            <AppShell/>
+        );
+    }
+}
+
+export default App;
